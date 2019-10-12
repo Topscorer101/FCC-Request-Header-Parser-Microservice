c3b18b018b32aad6dd38980066a62cb7c356a721
Topscorer101 <46632281+Topscorer101@users.noreply.github.com>
2019-10-12 17:03:45 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,29 @@
+ar express = require('express');
+var app = express();
 
+var cors = require('cors');
+const http = require('http');
+app.use(cors({optionSuccessStatus: 200})); 
+
+app.use(express.static('public'));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+app.get("/api/whoami", function (req, res) {
+  console.log(req.ip);
+  console.log(req.headers['accept-language']);
+  console.log(req.headers['user-agent']);
+  res.json({"ipaddress": req.ip,
+          "language": req.headers['accept-language'],
+          "software": req.headers['user-agent']});
+});
+
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
